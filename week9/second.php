7cb85838ed3ccfb1a861feb50fab9f08b5061e25
Amir <amir-cool@mail.ru>
2017-12-11 14:32:20 +0600

    task9

---
+++ week9/second.php
@@ -0,0 +1,29 @@
+<a href="second.php?category=sport">Sport news</a> | <a href="second.php?category=politics">Politic news</a><br/><br/>
+<a href="second.php?category=sport&format=json">Sport news (JSON)</a> | <a href="second.php?category=politics&format=json">Politic news (JSON)</a><br/><br/>
+
+<?php
+$news = ["sport"=>["C. Ronaldo has scored three goals in last five matches","Golovkin has won match for title"],"politics"=>["Trump has cancelled his visit to North Corea, because of sanction","N. Nazarbayev has approved new version of alphabet"]];
+
+//$category = ["sport"];
+$category=null;
+$format=null;
+//$category1=null;
+
+if(isset($_GET['category']) && isset($_GET['format'])){
+		$category=$_GET['category'];
+		echo json_encode($news[$category]);
+	
+}
+else if(isset($_GET['category'])){
+	$category=$_GET['category'];
+	foreach ($news[$category] as $new) {
+		echo $new;
+		echo "<div style='border:1px solid grey';></div>";
+
+
+	}
+
+}
+
+
+?>
