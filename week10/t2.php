211bf764a92f7e6948f58a82f502ded3664575b9
Amir <amir-cool@mail.ru>
2017-12-11 14:32:49 +0600

    task10

---
+++ week10/t2.php
@@ -0,0 +1,28 @@
+
+</form>
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		body{
+			background-color: #CFE2F3;
+		}
+		.a input {
+			background-color: grey;
+			width: 150px;
+			height: 30px;
+			border-radius: 5px;
+			color: black;
+			
+		}
+	</style>
+</head>
+<body>
+	<form action="t3.php">
+	<p>Lowest: <input type="search" name="low"></p>
+	<p>Highest: <input type="search" name="high"></p>
+	<div class="a">
+	<input type="submit" value="Search">
+	</div>
+</body>
+</html>
